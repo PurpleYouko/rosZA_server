b608180242f45bff6b77636165d4a702c268ee4f
purpleyouko <purpleyouko@gmail.com>
2017-05-23 10:45:09 -0500

    modified map_exp.sql

--- Database/map_exp.sql
+++ Database/map_exp.sql
@@ -1,200 +1,59 @@
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (1, 1, 'Canyon City of Zant');
+-- phpMyAdmin SQL Dump
+-- version 2.10.3
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: May 23, 2017 at 10:44 AM
+-- Server version: 5.0.45
+-- PHP Version: 5.2.3
 
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (2, 2, 'City of Junon Polis');
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (3, 21, 'Valley of Luxem Tower');
+-- 
+-- Database: `roseza`
+-- 
 
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (4, 22, 'Adventurer\'s Plains');
+-- --------------------------------------------------------
 
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (5, 23, 'Breezy Hills');
+-- 
+-- Table structure for table `map_exp`
+-- 
 
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (6, 24, 'El Verloon Desert');
+DROP TABLE IF EXISTS `map_exp`;
+CREATE TABLE IF NOT EXISTS `map_exp` (
+  `id` int(100) NOT NULL auto_increment,
+  `map_id` int(100) NOT NULL,
+  `map_name` varchar(100) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (7, 25, 'Anima Lake');
+-- 
+-- Dumping data for table `map_exp`
+-- 
 
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (8, 26, 'Forest of Wisdom');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (9, 27, 'Kenji Beach');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (10, 28, 'Gorge of Silence');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (11, 29, 'Desert of the Dead');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (12, 31, 'Goblin Cave (B1)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (13, 32, 'Goblin Cave (B2)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (14, 33, 'Goblin Cave (B3)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (15, 39, 'Snow Valley');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (16, 42, 'Oblivion Temple (B2)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (17, 43, 'Oblivion Temple (B3)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (18, 44, 'Oblivion Temple (B4)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (19, 51, 'Magic City of the Eucar');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (20, 52, 'Mana Snowfields');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (21, 53, 'Arumic Valley');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (22, 54, 'Crystal Snowfields');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (23, 55, 'Freezing Plateau');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (24, 56, 'Forgotten Temple (B1)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (25, 57, 'Forgotten Temple (B2)');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (26, 58, 'Mount Eruca');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (27, 59, 'Luna Clan Field');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (28, 61, 'Xita Refuge');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (29, 62, 'Shady Jungle');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (30, 63, 'Forest of Wandering');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (31, 64, 'Marsh of Ghosts');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (32, 65, 'Sikuku Underground Prison');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (33, 66, 'Sikuku Ruins');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (34, 74, 'Wreckage of The Blinding Mist');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (35, 75, 'The Golden Ring');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (36, 76, 'The Golden Ring');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (37, 77, 'The Golden Ring');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (38, 78, 'The Wasteland');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (39, 79, 'The Wasteland');
-
-INSERT INTO map_exp
-   (`id`, `map_id`, `map_name`)
-VALUES
-   (40, 81, 'Wasteland Ruins Path');
-
+INSERT INTO `map_exp` (`id`, `map_id`, `map_name`) VALUES 
+(1, 1, 'Canyon City of Zant'),
+(2, 2, 'City of Junon Polis'),
+(3, 21, 'Valley of Luxem Tower'),
+(4, 22, 'Adventurer''s Plains'),
+(5, 24, 'El Verloon Desert'),
+(6, 25, 'Anima Lake'),
+(7, 26, 'Forest of Wisdom'),
+(8, 27, 'Kenji Beach'),
+(9, 28, 'Gorge of Silence'),
+(10, 29, 'Desert of the Dead'),
+(11, 42, 'Oblivion Temple (B2)'),
+(12, 43, 'Oblivion Temple (B3)'),
+(13, 81, 'Wasteland Ruins Path'),
+(14, 52, 'Mana Snowfields'),
+(15, 53, 'Arumic Valley'),
+(16, 65, 'Sikuku Underground Prison'),
+(17, 62, 'Shady Jungle'),
+(18, 63, 'Forest of Wandering'),
+(19, 64, 'Marsh of Ghosts'),
+(20, 61, 'Xita Refuge'),
+(21, 66, 'Sikuku Ruins'),
+(22, 78, 'The Wasteland'),
+(23, 79, 'The Wasteland'),
+(24, 44, 'Oblivion Temple (B4)'),
+(25, 81, 'Wasteland Ruins Path');
